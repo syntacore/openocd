6240a44d36d9708582feefb73390aba82ba43c25
Evgeniy Naydanov <evgeniy.naydanov@syntacore.com>
2026-07-29 19:08:09 +0300

    Revert "gdb_server: Keep working if gdb requests a non-existent reg"
    This reverts commit 1c07a207e331ea9cfb1c16b2cb4d4c5fcf30a05a.
    
    The original commit seems like an ad-hoc fix for an issue that is not
    present anymore.
    
    Change-Id: If3ab19663a848ef40bd69aca1ef0e3c2dcf8ab9c
    Signed-off-by: Evgeniy Naydanov <evgeniy.naydanov@syntacore.com>

--- src/server/gdb_server.c
+++ src/server/gdb_server.c
@@ -1447,7 +1447,7 @@ static int gdb_get_register_packet(struct connection *connection,
 	if ((reg_list_size <= reg_num) || !reg_list[reg_num] ||
 		!reg_list[reg_num]->exist || reg_list[reg_num]->hidden) {
 		LOG_ERROR("gdb requested a non-existing register (reg_num=%d)", reg_num);
-		return gdb_error(connection, ERROR_FAIL);
+		return ERROR_SERVER_REMOTE_CLOSED;
 	}
 
 	reg_packet = calloc(DIV_ROUND_UP(reg_list[reg_num]->size, 8) * 2 + 1, 1); /* plus one for string termination null */
